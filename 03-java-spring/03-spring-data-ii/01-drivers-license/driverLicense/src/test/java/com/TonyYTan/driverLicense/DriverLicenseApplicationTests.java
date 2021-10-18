58c7526e7a2e8f5368499f5ef965d6b06d12c25e
TonyYTan <953243447@qq.com>
2021-10-17 21:26:38 -0400

    Driver License Assignment

---
+++ 03-java-spring/03-spring-data-ii/01-drivers-license/driverLicense/src/test/java/com/TonyYTan/driverLicense/DriverLicenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TonyYTan.driverLicense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverLicenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
